71f7ea3833bfb9b4c96891831481a5c9157c22a5
natejfoster <nathan.96@comcast.net>
2019-05-15 16:45:22 -0700

    No results state complete

--- src/App.js
+++ src/App.js
@@ -2,44 +2,13 @@ import React, { Component } from 'react';
 import Character from './components/character';
 import Search from './components/search';
 import './css/app.scss';
+import vader from './assets/vader-icon.svg';
 
 const baseURL = 'https://swapi.co/api/people/?search=';
-const test = {
-  "name": "Luke Skywalker",
-  "height": "172",
-  "mass": "77",
-  "hair_color": "blond",
-  "skin_color": "fair",
-  "eye_color": "blue",
-  "birth_year": "19BBY",
-  "gender": "male",
-  "homeworld": "https://swapi.co/api/planets/1/",
-  "films": [
-    "https://swapi.co/api/films/2/",
-    "https://swapi.co/api/films/6/",
-    "https://swapi.co/api/films/3/",
-    "https://swapi.co/api/films/1/",
-    "https://swapi.co/api/films/7/"
-  ],
-  "species": [
-    "https://swapi.co/api/species/1/"
-  ],
-  "vehicles": [
-    "https://swapi.co/api/vehicles/14/",
-    "https://swapi.co/api/vehicles/30/"
-  ],
-  "starships": [
-    "https://swapi.co/api/starships/12/",
-    "https://swapi.co/api/starships/22/"
-  ],
-  "created": "2014-12-09T13:50:51.644000Z",
-  "edited": "2014-12-20T21:17:56.891000Z",
-  "url": "https://swapi.co/api/people/1/"
-}
 
 class App extends Component {
   state = {
-    characters: {},
+    characters: [],
     query: 'Le',
     filter: ''
   }
@@ -47,21 +16,43 @@ class App extends Component {
   getResults = () => {
     fetch(baseURL + this.state.query)
       .then(res => res.json())
-      .then(data => console.log(data.results));
+      .then(data => {
+        this.setState({characters: data.results});
+      });
+  }
+
+  getQuery = () => {
+
   }
 
   render() { 
+    let results;
+
+    if (this.state.characters.length === 0) {
+      results = 
+        <div className='noResults'>
+          <div className='noResults__content'>
+            <img src={vader} alt='darth vader' className='noResults__icon'/>
+            <h2 className='noResults__desc'>Search by character name</h2> 
+          </div>
+        </div>
+    } else {
+      results = this.state.characters.map((character, index) => 
+        <Character 
+          key={index}
+          name={character.name}
+          gender={character.gender}
+          eye_color={character.eye_color}
+          hair_color={character.hair_color}
+        />
+      )
+    }
+
     return (
       <div className='App'>
-        <Search getResults={this.getResults}/>
+        <Search getResults={this.getResults} value={this.state.query}/>
         <div className='results'>
-          <Character 
-            key={1}
-            name={test.name}
-            gender={test.gender}
-            eye_color={test.eye_color}
-            hair_color={test.hair_color}
-          />
+          {results}
         </div>
       </div>
     );
--- src/components/search.js
+++ src/components/search.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import '../css/search.scss';
 
-const Search = ({getResults}) => {
+const Search = ({getResults, value}) => {
   return (
     <div className='search'>
       <input className='search__input' type='text' placeholder='Search by character name'></input>
-      <button className='search__btn' onClick={getResults}>Search</button>
+      <button className='search__btn' onClick={getResults} value={value}>Search</button>
     </div>
   );
 }

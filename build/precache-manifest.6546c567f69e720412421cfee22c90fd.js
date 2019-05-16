845aa605e7871368ccb5f3ec9343e9be7204419a
natejfoster <nathan.96@comcast.net>
2019-05-16 15:15:35 -0700

    fixed vertical spacing on filter section

---
+++ build/precache-manifest.6546c567f69e720412421cfee22c90fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29ab2e644dda6d26e97b307da7f4b8b9",
+    "url": "/tech-challenge/index.html"
+  },
+  {
+    "revision": "f3e879c8b80766aa1c44",
+    "url": "/tech-challenge/static/css/main.6d6d6fd9.chunk.css"
+  },
+  {
+    "revision": "43d1f55c2a696b4563b8",
+    "url": "/tech-challenge/static/js/2.179f4c70.chunk.js"
+  },
+  {
+    "revision": "f3e879c8b80766aa1c44",
+    "url": "/tech-challenge/static/js/main.bd08b093.chunk.js"
+  },
+  {
+    "revision": "65f89f93ba52fc3c9e3b",
+    "url": "/tech-challenge/static/js/runtime~main.657c240c.js"
+  },
+  {
+    "revision": "0ed616d70f0efcf679cac7630414657d",
+    "url": "/tech-challenge/static/media/chevron--down.0ed616d7.svg"
+  },
+  {
+    "revision": "1e934ee59af004264a0b97f4a176b901",
+    "url": "/tech-challenge/static/media/vader-icon.1e934ee5.svg"
+  }
+]);
